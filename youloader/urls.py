80f9de64af2ce79f5431ff33008e0cb49364536a
Jackson Linus <jacksonlinus95@gmail.com>
2021-01-01 14:05:00 +0300

    Video downloading function

--- youloader/urls.py
+++ youloader/urls.py
@@ -3,9 +3,10 @@ from youloader import views
 
 
 urlpatterns = [
-	path('',views.index),
-	path('video/<str:id>/',views.video_details, name='video_details'),
-	path('download/<str:id>/',views.download_audio, name='download_audio')
+    path('',views.index),
+    path('video/<str:id>/',views.video_details, name='video_details'),
+    path('get_audio/<str:id>/',views.download_audio, name='download_audio'),
+    path('get_video/<str:id>/',views.download_video, name='download_video'),
     # path(r'^home/download/$',views.download),
     # path(r'home/downloading/$',views.downloading),
 ]
--- youloader/views.py
+++ youloader/views.py
@@ -25,23 +25,28 @@ def my_hook(d):
 
 
 audio_opts = {
+    'writethumbnail': True,
     'format': 'bestaudio/best',
     'outtmpl': '/downloads/audios/%(title)s.%(ext)s',
-    'postprocessors': [{
+    'postprocessors': [
+    {
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
+        'preferredquality': '192',},
+        {'key': 'EmbedThumbnail'},
+        {'key': 'FFmpegMetadata'},
+
+    ],
     'logger': MyLogger(),
     'progress_hooks': [my_hook],
 }
 
 
-
-
 video_opts = {
-    'format':'worstvideo[ext=mp4]+worstaudio', #during testing to reduce data consumption
+    # 'format':'worstvideo[ext=mp4]+worstaudio', #during testing to reduce data consumption
     # 'format':'bestvideo+bestaudio', #to be used in production environment
+    'format':'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+    'outtmpl': '/downloads/videos/%(title)s.%(ext)s',
     'embed-thumbnail':'True',
     'write-description':'True',
     'write-info-json':'True',
@@ -52,24 +57,16 @@ video_opts = {
     }
 
 
-
-
 def index(request):
 
     if request.method == 'POST':
         query = request.POST['video_name']
         n = 12
         search = SearchVideos(str(query), offset = 1, mode = "json", max_results = n)
-
         ytresults = search.result()
-
         result_dict = json.loads(ytresults)
-
-
         context = {
-            "result" : result_dict,
-        
-                
+            "result" : result_dict, 
         }
         template_name = "youloader/results.html"
         return render(request, template_name, context)
@@ -77,86 +74,61 @@ def index(request):
     else:
         query = "no copyright sound"
         n = 12
-
         search = SearchVideos(str(query), offset = 1, mode = "json", max_results = n)
-
         index_results = search.result()
-
         result_dict = json.loads(index_results)
-
-
         context = {
             "result" : result_dict
         }
-
         template_name = "youloader/index.html"
         return render(request, template_name, context)
 
 
-
-
 def video_details(request, id):
     search = SearchVideos(str(id), offset = 1, mode = "json", max_results = 1)
-
     ytresults = search.result()
-
     result_dict = json.loads(ytresults)
-
     link = (result_dict['search_result'][0]['link'])
-
     # print(link)
-
-
     context = {
         "result" : result_dict,
-    
-            
     }
     template_name = "youloader/video_details.html"
     return render(request, template_name, context)
 
 
-
-
 def download_audio(request, id):
     search = SearchVideos(str(id), offset = 1, mode = "json", max_results = 1)
-
     ytresults = search.result()
-
     result_dict = json.loads(ytresults)
-
     link = (result_dict['search_result'][0]['link'])
-
-
     # print(link)
-
-    # return HttpResponse(link)
-
+    # return HttpResponse("audio")
     try:
       with youtube_dl.YoutubeDL(audio_opts) as ydl_audio:
         ydl_audio.download([link])
 
-        return HttpResponse('Downloading..............')
+        return HttpResponse('Audio downloaded!')
 
     except:
         return HttpResponse('failed to download! ')
 
-        # try:
-        #    with youtube_dl.YoutubeDL(audio_opts) as ydl:
-        #        ydl.download(link)
-
-        # except:
-        #     pass
 
 
+def download_video(request, id):
+    search = SearchVideos(str(id), offset = 1, mode = "json", max_results = 1)
+    ytresults = search.result()
+    result_dict = json.loads(ytresults)
+    link = (result_dict['search_result'][0]['link'])
+    # print(link)
+    # return HttpResponse("video")
+    try:
+      with youtube_dl.YoutubeDL(video_opts) as ydl_audio:
+        ydl_audio.download([link])
 
-# def download_audio(request):
-#     if request.method == 'POST':
-#         url = request.POST.get('url')
+        return HttpResponse('Video downloaded!')
 
-#         print(url)
-
-
-
+    except:
+        return HttpResponse('failed to download! ')
 
 
